6e4a972e48b97c78765667229f07a8027384425d
Eduard <evlisouski@gmail.com>
2024-04-26 12:34:18 +0300

    fix

--- ubuntu-dev-setup.sh
+++ ubuntu-dev-setup.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-echo "Turn off screensaver"
-current=$(gsettings get org.gnome.desktop.session idle-delay | cut -d' ' -f2)
-if [ $current -ne 0 ]; then
-	gsettings set org.gnome.desktop.session idle-delay 0
-	echo "idle-delay set to 0"
-else
-	echo "idle-delay alredy set to 0"
-fi
-
 sudo apt update && sudo apt upgrade -y
 
-#sudo apt install open-vm-tools-desktop -y
 sudo apt install -y python3-pip
 sudo apt install -y python3-venv
-
 sudo apt install git -y
+
+# start jetbrains toolbox
+sudo apt install libfuse2
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.3.0.30876.tar.gz
+sudo tar -xvzf jetbrains-toolbox-2.3.0.30876.tar.gz -C /opt
+sudo /opt/jetbrains-toolbox-2.3.0.30876/jetbrains-toolbox
+sudo rm -frd jetbrains-toolbox-2.3.0.30876.tar.gz
+# end jetbrains toolbox
+
 sudo snap install code --classic
 sudo snap install pycharm-community --classic
-sudo snap install postman
+sudo snap install postman -y
 
-echo "Docker installation"
+
+echo "---------------------------------------"
+echo "Start Docker installation"
+echo "---------------------------------------"
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -36,8 +37,9 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo docker run hello-world
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "---------------------------------------"
+echo "Docker Installed"
+echo "---------------------------------------"
 
 
