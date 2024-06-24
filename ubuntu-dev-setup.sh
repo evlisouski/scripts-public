d757b917cf83101fbd85a639fce4c4d5110b9300
Eduard <evlisouski@gmail.com>
2024-06-24 10:41:32 +0300

    fix: fix jetbrain toolbox instalation

--- ubuntu-dev-setup.sh
+++ ubuntu-dev-setup.sh
@@ -5,13 +5,36 @@ sudo apt install -y python3-pip
 sudo apt install -y python3-venv
 sudo apt install git -y
 
-# start jetbrains toolbox
+# REGION jetbrains toolbox installation
 sudo apt install libfuse2
 wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.3.0.30876.tar.gz
-sudo tar -xvzf jetbrains-toolbox-2.3.0.30876.tar.gz -C /opt
-sudo /opt/jetbrains-toolbox-2.3.0.30876/jetbrains-toolbox
-sudo rm -frd jetbrains-toolbox-2.3.0.30876.tar.gz
-# end jetbrains toolbox
+tar -xvzf jetbrains-toolbox-2.3.0.30876.tar.gz
+sudo mv jetbrains-toolbox-2.3.0.30876 /opt/jetbrains-toolbox
+## create symbol link (jetbrains-toolbox)
+sudo ln -s /opt/jetbrains-toolbox/jetbrains-toolbox /usr/local/bin/jetbrains-toolbox
+## region create desktop shortcut
+DESKTOP_FILE_CONTENT="[Desktop Entry]
+Version=1.0
+Type=Application
+Name=JetBrains Toolbox
+Exec=/opt/jetbrains-toolbox/jetbrains-toolbox
+Icon=/opt/jetbrains-toolbox/jetbrains-toolbox.png
+Comment=JetBrains Toolbox
+Categories=Development;
+Terminal=false"
+DESKTOP_FILE_PATH="/usr/share/applications/jetbrains-toolbox.desktop"
+AUTOSTART_DIR="$HOME/.config/autostart"
+AUTOSTART_LINK_PATH="$AUTOSTART_DIR/jetbrains-toolbox.desktop"
+
+echo "$DESKTOP_FILE_CONTENT" | sudo tee $DESKTOP_FILE_PATH > /dev/null
+sudo chmod 644 $DESKTOP_FILE_PATH
+mkdir -p $AUTOSTART_DIR
+ln -s $DESKTOP_FILE_PATH $AUTOSTART_LINK_PATH
+
+echo "Desktop entry created at $DESKTOP_FILE_PATH"
+echo "Autostart entry created at $AUTOSTART_LINK_PATH"
+## endregion create desktop shortcut
+# ENDREGION jetbrains toolbox installation
 
 sudo snap install code --classic
 sudo snap install pycharm-community --classic
